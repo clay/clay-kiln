8bc9741a045dd6817670b13b03a48296192cd617
Nelson Pecora <nelson@yoshokatana.com>
2015-05-27 17:41:38 -0400

    simple list is working

--- behaviors/simple-list.js
+++ behaviors/simple-list.js
@@ -1,17 +1,67 @@
 'use strict';
 var _ = require('lodash'),
+  keycode = require('keycode'),
   dom = require('../services/dom');
 
 module.exports = function (result, args) {
-  var min = args.min,
-    max = args.max,
-    listTpl = `
+  // todo: args.min is a thing
+  var listTpl = `
     <section name="${result.bindings.name}" class="simple-list" rv-simplelist="data">
-      <span rv-each-item="data" class="simple-list-item" rv-class-primary="item.primary" contenteditable="false">{ item.text }</span>
-      <span class="simple-list-add" contenteditable="true"></span>
+      <span tabindex="0" rv-each-item="data" class="simple-list-item" rv-class-selected="item.selected" rv-on-click="selectItem" rv-on-keydown="keyactions">{ item.text }</span>
+      <span class="simple-list-add" rv-on-click="unselectAll" contenteditable="true"></span>
     </section>`,
     el = dom.create(listTpl);
 
+  function unselectAll(items) {
+    return items.map(function (item) {
+      item.selected = false;
+      return item;
+    });
+  }
+
+  function selectItem(bindings) {
+    var item = bindings.item,
+      data = bindings.data;
+
+    unselectAll(data);
+    item.selected = true;
+  }
+
+  // unselect all items when you click to add a new one
+  result.bindings.unselectAll = function (e, bindings) {
+    unselectAll(bindings.data);
+  };
+
+  // select an item (and unselect all others) when you click it
+  result.bindings.selectItem = function (e, bindings) {
+    selectItem(bindings);
+  };
+
+  // move between items and delete items when pressing the relevant keys (when an item is selected)
+  result.bindings.keyactions = function (e, bindings) {
+    var key = keycode(e),
+      index = bindings.data.indexOf(bindings.item);
+
+    if (key === 'left' && index > 0) {
+      selectItem({ item: bindings.data[index - 1], data: bindings.data });
+      e.target.previousSibling.focus();
+    } else if ((key === 'tab' || key === 'right') && index < bindings.data.length) {
+      selectItem({ item: bindings.data[index + 1], data: bindings.data });
+      e.target.nextSibling.focus();
+    } else if (key === 'delete' || key === 'backspace') {
+      e.preventDefault();
+      bindings.data.splice(index, 1); // remove item from the list
+
+      // select item, or focus the input
+      if (index < bindings.data.length) {
+        selectItem({ item: bindings.data[index + 1], data: bindings.data });
+      } else {
+        dom.find(el, '.simple-list-add').focus();
+      }
+    }
+  };
+
+  // put the element into the result object
   result.el = el;
 
   // add binder for creating new items
@@ -37,31 +87,29 @@ module.exports = function (result, args) {
         adapter.set(model, keypath, data); // update!
       }
 
-      // add keyboard events to "add item" span
+      // select the last item when you backspace from the add-items field
+      function selectLastItem() {
+        var data = adapter.get(model, keypath);
+
+        _.last(data).selected = true;
+        adapter.set(model, keypath, data); // update!
+        _.last(items).focus();
+      }
+
+      // add keyboard events to add-item field
       addEl.addEventListener('keyup', function (e) {
-        if (
-          e.keyCode === 9 || // tab
-          e.keyCode === 13 // enter
-        ) {
-          // create new item when pressing tab or enter
+        var key = keycode(e);
+
+        if (key === 'enter' || key === 'tab') {
+          // add new item
           addItem(e);
-        } else if (
-          e.keyCode === 8 || // delete
-          e.keyCode === 46 // backspace
-        ) {
-          // if there's nothing here, select the last tag
-          // if (addEl.innerText === '') {
-          //   selectItem(_.last(data));
-          // }
+        } else if (key === 'delete' || key === 'backspace') {
+          // select last item
+          if (!addEl.innerText.length) {
+            selectLastItem();
+          }
         }
       });
-    },
-
-    unbind: function () {
-    },
-
-    routine: function (el, value) {
-      console.log(value)
     }
   };
 
