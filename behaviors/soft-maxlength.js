989d0b5a88ed75f4c6e0f2290355d91fd51c0729
Nelson Pecora <nelson@yoshokatana.com>
2015-06-10 11:33:55 -0400

    fixed eslint issues with soft-maxlength and medium-button

--- behaviors/soft-maxlength.js
+++ behaviors/soft-maxlength.js
@@ -14,20 +14,15 @@ module.exports = function (result, args) {
   rivets.formatters.charsRemaining = function (max, data) {
     var length = parseInt(striptags(data.value).length),
       remaining = max - length;
-      // input = dom.find(el, 'input'),
-      // label = dom.find(el, '.label-inner').innerText;
 
     if (remaining > 0) {
       span.classList.remove('too-long');
-      // input.setCustomValidity('');
       return remaining + ' characters remaining';
     } else if (remaining === 0) {
       span.classList.remove('too-long');
-      // input.setCustomValidity('');
       return -remaining + ' characters over limit!';
     } else {
       span.classList.add('too-long');
-      // input.setCustomValidity(label + ' is too long!');
       return -remaining + ' characters over limit!';
     }
   };
--- services/medium-button.js
+++ services/medium-button.js
@@ -3,18 +3,17 @@
  *   MIT (c) Patrick Stillhart
  *   https://github.com/arcs-/MediumButton
  */
-'use strict';
 
 function getCurrentSelection() {
   var html = '',
     sel, container, i, len;
-  
+
   if (typeof window.getSelection !== 'undefined') {
     sel = window.getSelection();
-    
+
     if (sel.rangeCount) {
       container = document.createElement('div');
-      
+
       for (i = 0, len = sel.rangeCount; i < len; ++i) {
         container.appendChild(sel.getRangeAt(i).cloneContents());
       }
@@ -26,45 +25,39 @@ function getCurrentSelection() {
       html = document.selection.createRange().htmlText;
     }
   }
-  
+
   return html;
 }
 
 function MediumButton(options) {
-  if (options.label === undefined || !/\S{1}/.test(options.label) || 
-    options.start === undefined || !/\S{1}/.test(options.start) || 
-    options.end === undefined || !/\S{1}/.test(options.end)) {
-    
-    if(options.label === undefined || !/\S{1}/.test(options.label) ||
-       options.action === undefined || !/\S{1}/.test(options.action)) {
-      console.error('[Custom-Button] You need to specify "label", "start" and "end" OR "label" and "action"');  
-      return;
-    } 
+  if (!options.label || !options.start && !options.end || !options.label && !options.action) {
+    console.error('[Custom-Button] You need to specify "label", "start" and "end" OR "label" and "action"');
+    return;
   }
-  
-  options.start = (options.start === undefined) ? '' : options.start;
-  options.end = (options.end === undefined) ? '' : options.end;
-  
+
+  options.start = options.start || '';
+  options.end = options.end || '';
+
   this.options = options;
   this.button = document.createElement('button');
   this.button.className = 'medium-editor-action';
   this.button.innerHTML = options.label;
-  this.button.onclick = function() {
+  this.button.onclick = function () {
     // Get Current Value
     var html = getCurrentSelection(),
       sel = window.getSelection(),
       range, fragment, firstInsertedNode, lastInsertedNode;
-      
-    // Modify Content 
+
+    // Modify Content
     if (options.start === undefined || html.indexOf(options.start) === -1 && html.indexOf(options.end) === -1) {
-      if(options.action !== undefined) {
+      if (options.action !== undefined) {
         html = options.action(html, true);
       }
-      
+
       html = options.start + html + options.end;
 
-    } else { //clean old
-      if(options.action !== undefined) {
+    } else { // clean old
+      if (options.action !== undefined) {
         html = options.action(html, false);
       }
 
@@ -73,7 +66,7 @@ function MediumButton(options) {
       html = html.split(options.end).join('');
     }
 
-    //Set new Content
+    // Set new Content
     if (sel.getRangeAt && sel.rangeCount) {
       range = window.getSelection().getRangeAt(0);
       range.deleteContents();
@@ -86,8 +79,8 @@ function MediumButton(options) {
       range.insertNode(fragment);
 
       if (firstInsertedNode) {
-          range.setStartBefore(firstInsertedNode);
-          range.setEndAfter(lastInsertedNode);
+        range.setStartBefore(firstInsertedNode);
+        range.setEndAfter(lastInsertedNode);
       }
       sel.removeAllRanges();
       sel.addRange(range);
@@ -95,14 +88,15 @@ function MediumButton(options) {
   };
 }
 
-MediumButton.prototype.getButton = function() {
-    return this.button;
+MediumButton.prototype.getButton = function () {
+  return this.button;
 };
 
-MediumButton.prototype.checkState = function() {
+MediumButton.prototype.checkState = function () {
   var html = getCurrentSelection();
+
   if (this.options.start !== '' && html.indexOf(this.options.start) > -1 && html.indexOf(this.options.end) > -1) {
-      this.button.classList.add('medium-editor-button-active');
+    this.button.classList.add('medium-editor-button-active');
   }
 };
 
