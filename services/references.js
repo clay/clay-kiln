1e63b79922d6987bdc30dad58cda51602015ad0f
Nelson Pecora <nelson@yoshokatana.com>
2015-08-04 10:51:17 -0400

    added placeholderAttribute

--- services/references.js
+++ services/references.js
@@ -1,6 +1,7 @@
 module.exports = {
   componentAttribute: 'data-component', // deprecated: indicates element is the container of a component
   editableAttribute: 'data-editable', // indicates el is editable when clicked. value is path to data
+  placeholderAttribute: 'data-placeholder', // indicates el should have placeholder, but is NOT editable when clicked
   fieldAttribute: 'data-field', // indicates el is a field in an open form. value is path to data
   referenceAttribute: 'data-ref', // indicates el is the container of a component. value is _ref of component instance
   referenceProperty: '_ref',
