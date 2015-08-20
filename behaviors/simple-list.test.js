a009e831f1ea0ddd02ced4e65307316a8e487ea9
Nelson Pecora <nelson@yoshokatana.com>
2015-08-20 15:39:41 -0400

    testing rivets doesn't work

--- behaviors/simple-list.test.js
+++ behaviors/simple-list.test.js
@@ -5,7 +5,6 @@ var dirname = __dirname.split('/').pop(),
   references = require('../services/references'),
   rivets = require('rivets'),
   _ = require('lodash'),
-  dom = require('../services/dom'),
   data = [{
     text: 'foo'
   }, {
@@ -50,120 +49,6 @@ describe(dirname, function () {
         expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
         expect(el.querySelectorAll('.simple-list-item').length).to.equal(2);
       });
-
-      it('selects on click', function () {
-        var firstItem = dom.find(el, '.simple-list-item'),
-          e = document.createEvent('MouseEvent'),
-          items;
-
-        function testEvent(e) {
-          firstItem.dispatchEvent(e);
-          items = findBinding('foo.data', view).observer.value();
-          expect(items[0]).to.eql({ text: 'foo', _selected: true});
-          expect(items[1]).to.eql({ text: 'bar', _selected: false});
-        }
-
-        e.initEvent('click', true, true);
-
-        _.defer(testEvent.bind(null, e));
-      });
-
-      it('selects previous item on left keypress');
-
-      it('selects next item on right or tab keypress');
-
-      it('deletes item on delete or backspace keypress');
-
-      it('selects previous item on delete (if a previous item exists)');
-
-      it('focuses input on delete (if no previous items exist)');
-    });
-  });
-
-  describe('simplelist binder', function () {
-    var result = lib(fixture),
-      el = result.el,
-      bindings = { foo: result.bindings },
-      addEl, view;
-
-    beforeEach(function () {
-      _.assign(rivets.binders, result.binders);
-      view = rivets.bind(el, bindings);
-      addEl = dom.find(el, '.simple-list-add');
-    });
-
-    afterEach(function () {
-      view.unbind();
-    });
-
-    it('adds item on enter if text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = 'hello';
-    });
-
-    it('adds item on tab if text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = 'hello';
-    });
-
-    it('adds item on comma if text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = 'hello';
-    });
-
-    it('closes itself on enter if no text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = '';
-    });
-
-    it('closes itself on tab if no text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = '';
-    });
-
-    it('closes itself on comma if no text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-    });
-
-    it('selects last item on delete if no text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = '';
-    });
-
-    it('selects last item on backspace if no text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = '';
-    });
-
-    it('selects last item on left if no text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = '';
-    });
-
-    it('does not select last item on delete if text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = 'hello';
-    });
-
-    it('does not select last item on backspace if text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = 'hello';
-    });
-
-    it('does not select last item on left if text', function () {
-      expect(findBinding('foo.data', view).observer.value().length).to.equal(2);
-
-      addEl.value = 'hello';
     });
   });
 });
