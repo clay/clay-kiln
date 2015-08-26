38a87e25ed7493cd3ab24d673cd68cf695ebaf5f
Morgan Croney <m@morgancroney.com>
2015-08-26 08:46:29 -0400

    Remove console logs.

--- behaviors/component-ref.js
+++ behaviors/component-ref.js
@@ -11,19 +11,15 @@ var _ = require('lodash'),
   references = require('../services/references');
 
 module.exports = function (result, args) {
-  var hiddenInput, name = result.name;
+  var hiddenInput,
+    name = result.name;
 
   if (args.selector) {
-    console.log(args.selector);
-    console.log(document.querySelectorAll(args.selector));
     result.bindings.data.value = _.map(document.querySelectorAll(args.selector), function (el) {
       return el.getAttribute(references.referenceAttribute);
     });
-    console.log(result.bindings.data.value);
     hiddenInput = dom.create(`<input type="hidden" class="input-text" rv-field="${name}" rv-value="${name}.data.value">`);
-    console.log(hiddenInput);
     result.el.appendChild(hiddenInput);
-    console.log(result.el);
   }
   return result;
 };
