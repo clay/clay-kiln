7ba3872d2ac0fc4885be2c2b21b0e51c1f5328ba
Morgan Croney <m@morgancroney.com>
2015-09-01 11:55:36 -0400

    Select tests and jsdocs.

--- behaviors/select.js
+++ behaviors/select.js
@@ -1,30 +1,33 @@
-/*
-Select arguments
-
-options {array} array of strings
- */
-
 var _ = require('lodash'),
   dom = require('../services/dom');
 
+/**
+ * Create options for select.
+ * @param {[string]} options
+ * @returns {string}
+ */
 function createOptions(options) {
   return options.map(function (option) {
     return `<option value="${option}">${ _.startCase(option) || 'None' }</option>`;
   }).join('\n');
 }
 
+/**
+ * Replace result.el with select drop-down of options
+ * @param {{name: string}} result
+ * @param {{options: [string]}} args
+ * @returns {{}}
+ */
 module.exports = function (result, args) {
   var name = result.name,
     options = args.options,
-    field;
-
-  field = dom.create(`
-    <label class="input-label">
-      <select class="editor-select" rv-field="${name}" rv-value="${name}.data.value">
-        ${ createOptions(options) }
-      </select>
-    </label>
-  `);
+    field = dom.create(`
+      <label class="input-label">
+        <select class="editor-select" rv-field="${name}" rv-value="${name}.data.value">
+          ${ createOptions(options) }
+        </select>
+      </label>
+    `);
 
   result.el = field;
 
