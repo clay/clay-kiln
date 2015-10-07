41defaaee233d646dc6efea4fd8c2673fb15c15d
Nelson Pecora <nelson@yoshokatana.com>
2015-10-07 12:18:31 -0400

    added failing test

--- services/label.test.js
+++ services/label.test.js
@@ -8,6 +8,12 @@ describe('label service', function () {
     expect(label(name, schema)).to.equal('Baz');
   });
 
+  it('doesn\'t have "Clay " in front of every npm component\'s label', function () {
+    var name = 'clay-foo-bar';
+
+    expect(label(name)).to.equal('Foo Bar');
+  });
+
   it('falls back to prettified name if no _label', function () {
     var name = 'foo-bar';
 
