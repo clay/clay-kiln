882c9c4bf01819e64cb1a4e89de800a1b733173f
Nelson Pecora <nelson@yoshokatana.com>
2015-12-08 16:55:53 -0500

    updated checkbox styles

--- behaviors/checkbox.js
+++ behaviors/checkbox.js
@@ -3,13 +3,15 @@ var dom = require('../services/dom');
 /**
  * Replace result.el with checkbox.
  * @param {{name: string}} result
+ * @param {{label: string}} args
  * @returns {{}}
  */
-module.exports = function (result) {
+module.exports = function (result, args) {
   var name = result.name,
+    label = args.label,
     tpl = `
       <label class="input-label">
-        <input class="input-checkbox" rv-field="${name}" type="checkbox" rv-checked="${name}.data.value" />
+        <input class="input-checkbox" rv-field="${name}" type="checkbox" rv-checked="${name}.data.value" /><span class="checkbox-label">${label}</span>
       </label>
     `,
     checkbox = dom.create(tpl);
