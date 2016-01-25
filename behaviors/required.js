8049cf8d7b997009b59ddd0534d3d3f960d8712e
Nelson Pecora <nelson@yoshokatana.com>
2016-01-25 16:48:42 -0500

    add visual indicator for required

--- behaviors/required.js
+++ behaviors/required.js
@@ -1,9 +1,20 @@
-/*
- * The literal concept of emptiness.  Later, will show when something is feeling empty.
- *
- * @module
- */
+var dom = require('../services/dom');
 
+/**
+ * Add a "required" bit of text after the label (if it exists)
+ * @param {{name: string, el: Element}} result
+ * @returns {{}}
+ */
 module.exports = function (result) {
+  var el = result.el,
+    tpl = `<span class="label-required">required</span>`,
+    requiredEl = dom.create(tpl),
+    label = dom.find(el, '.label-inner');
+
+  // only add "required" if there's a label
+  if (label) {
+    dom.insertAfter(label, requiredEl);
+  }
+
   return result;
 };
