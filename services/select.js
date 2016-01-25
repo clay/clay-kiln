b6f59f0aa3c3fcca89bbba014b97c84cbadd34cb
Nelson Pecora <nelson@yoshokatana.com>
2016-01-25 17:45:11 -0500

    stop scrolling if user manually scrolls

--- services/select.js
+++ services/select.js
@@ -243,9 +243,23 @@ function scrollToY(scrollTargetY, speed, easing) {
   let time = Math.max(0.1, Math.min(Math.abs(scrollY - scrollTargetY) / speed, 0.8)),
     currentTime = 0;
 
+  // defaults
   scrollTargetY = scrollTargetY || 0;
   speed = speed || 2000;
   easing = easing || 'easeOutSine';
+
+  function stopAutoScroll() {
+    window.autoScroll = false;
+    window.removeEventListener('wheel', stopAutoScroll);
+    window.removeEventListener('mousewheel', stopAutoScroll);
+    window.removeEventListener('touchmove', stopAutoScroll);
+  }
+
+  // set a scroll handler so we can stop tick()'ing if the user manually scrolls
+  window.autoScroll = true;
+  window.addEventListener('wheel', stopAutoScroll); // firefox
+  window.addEventListener('mousewheel', stopAutoScroll); // chrome, safari, etc
+  window.addEventListener('touchmove', stopAutoScroll); // ios, android
 
   // recursive animation loop
   function tick() {
@@ -255,12 +269,15 @@ function scrollToY(scrollTargetY, speed, easing) {
     p = currentTime / time;
     t = easingEquations[easing](p);
 
-    if (p < 1) {
+    if (p < 1 && window.autoScroll) {
+      // keep scrolling
       window.requestAnimationFrame(tick);
-
       window.scrollTo(0, scrollY + ((scrollTargetY - scrollY) * t));
+      window.autoScroll = true;
+    } else if (!window.autoScroll) {
+      // user manually stopped the scroll animation
     } else {
-      console.log('scroll done');
+      // scroll the rest of the way to the top
       window.scrollTo(0, scrollTargetY);
     }
   }
