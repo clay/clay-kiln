e4110b7933d3452019baff63c218c1c6542abf53
Nelson Pecora <nelson@yoshokatana.com>
2016-04-14 12:14:13 -0400

    treat simple-list like inputs (#422)
    * treat simple-list like inputs
    
    * updated tests

--- services/get-input.js
+++ services/get-input.js
@@ -2,11 +2,11 @@
 var dom = require('@nymag/dom');
 
 function getInput(el) {
-  return dom.find(el, '.checkbox-group') || dom.find(el, 'input') || dom.find(el, 'textarea') || dom.find(el, '.wysiwyg-input');
+  return dom.find(el, '.checkbox-group') || dom.find(el, '.simple-list') || dom.find(el, 'input') || dom.find(el, 'textarea') || dom.find(el, '.wysiwyg-input');
 }
 
 function isInput(el) {
-  return !!el && (el.nodeName === 'INPUT' || el.nodeName === 'TEXTAREA' || el.classList.contains('wysiwyg-input'));
+  return !!el && (el.nodeName === 'INPUT' || el.nodeName === 'TEXTAREA' || el.classList.contains('wysiwyg-input') || el.classList.contains('simple-list'));
 }
 
 module.exports = getInput;
