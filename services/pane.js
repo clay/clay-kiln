65289fd9891e29d5dbb6eb83eeb6c059a5bba448
David Andrew Frey <davidandrewfrey@gmail.com>
2016-04-21 17:04:00 -0400

    Merge pull request #433 from nymag/new-page-type-selector
    New page type selector

--- services/pane.js
+++ services/pane.js
@@ -5,6 +5,7 @@ var _ = require('lodash'),
   state = require('./page-state'),
   site = require('./site'),
   paneController = require('../controllers/pane'),
+  newPagePaneController = require('../controllers/pane-new-page'),
   publishPaneController = require('../controllers/publish-pane');
 
 /**
@@ -63,25 +64,23 @@ function open(header, innerEl) {
  * @returns {Element}
  */
 function createPublishMessages(res) {
-  var messages = dom.create('<div class="messages"></div>'),
-    draftMessage = dom.create('<p>In Draft.</p>'),
-    effectMessage = dom.create('<p>Changes you make outside of the article will also be published.</p>');
+  var messages = dom.find('.publish-messages'),
+    scheduleMessage, stateMessage;
 
-  // add messages (in order)
   if (res.published) {
-    // published message
-    messages.appendChild(dom.create(`<p>Published ${state.formatTime(res.publishedAt)}</p>`)); // todo: get published time
-  } else {
-    messages.appendChild(draftMessage);
+    stateMessage = dom.find(messages, '.publish-state-message');
+    if (stateMessage) {
+      stateMessage.innerHTML = `Published ${state.formatTime(res.publishedAt)}`;
+    }
   }
 
   if (res.scheduled) {
-    // scheduled message
-    messages.appendChild(dom.create(`<p>Scheduled to publish ${state.formatTime(res.scheduledAt)}.</p>`));
+    scheduleMessage = dom.find(messages, '.publish-schedule-message');
+    if (scheduleMessage) {
+      scheduleMessage.innerHTML = `Scheduled to publish ${state.formatTime(res.scheduledAt)}.`;
+      scheduleMessage.classList.remove('hide');
+    }
   }
-
-  // message about what publishing affects
-  messages.appendChild(effectMessage);
 
   return messages;
 }
@@ -92,36 +91,40 @@ function createPublishMessages(res) {
  * @returns {Element}
  */
 function createPublishActions(res) {
-  var today = moment().format('YYYY-MM-DD'),
-    now = moment().format('HH:mm'),
-    actions = dom.create('<div class="actions"></div>'),
-    schedule = dom.create(`<form class="schedule">
-      <label class="schedule-label" for="schedule-date">Date</label>
-      <input id="schedule-date" class="schedule-input" type="date" min="${today}" value="${today}" placeholder="${today}"></input>
-      <label class="schedule-label" for="schedule-time">Time</label>
-      <input id="schedule-time" class="schedule-input" type="time" value="${now}" placeholder="${now}"></input>
-      <button class="schedule-publish">Schedule Publish</button>
-    </form>`),
-    unschedule = dom.create('<button class="unschedule">Unschedule</button>'),
-    publishNow = dom.create('<button class="publish-now">Publish Now</button>'),
-    unpublish = dom.create('<button class="unpublish">Unpublish Page</button>');
+  const actions = dom.find('.publish-actions'),
+    today = moment().format('YYYY-MM-DD'),
+    now = moment().format('HH:mm');
+  let scheduleDate, scheduleTime, unpublish, unschedule;
 
-  // add actions and buttons (in order)
-  // scheduling: always exists
-  actions.appendChild(schedule);
+  // set date and time
+  if (actions) {
+    scheduleDate = dom.find(actions, '#schedule-date');
+    scheduleTime = dom.find(actions, '#schedule-time');
+
+    if (scheduleDate) {
+      dom.replaceElement(scheduleDate, dom.create(`<input id="schedule-date" class="schedule-input" type="date" min="${today}" value="${today}" placeholder="${today}"></input>`));
+    }
+
+    if (scheduleTime) {
+      dom.replaceElement(scheduleTime, dom.create(`<input id="schedule-time" class="schedule-input" type="time" value="${now}" placeholder="${now}"></input>`));
+    }
+  }
 
   // unscheduling
   if (res.scheduled) {
     state.toggleScheduled(true); // just in case someone else scheduled this page
-    actions.appendChild(unschedule);
+    unschedule = dom.find(actions, '.unschedule');
+    if (unschedule) {
+      unschedule.classList.remove('hide');
+    }
   }
-
-  // publish: always exists
-  actions.appendChild(publishNow);
 
   // unpublish (only affects page)
   if (res.published) {
-    actions.appendChild(unpublish);
+    unpublish = dom.find(actions, '.unpublish');
+    if (unpublish) {
+      unpublish.classList.remove('hide');
+    }
   }
 
   return actions;
@@ -149,6 +152,24 @@ function openPublish() {
   });
 }
 
+/**
+ * open new page type dialog pane
+ * @returns {Promise}
+ */
+function openNewPage() {
+  var header = 'New Page',
+    innerEl = document.createDocumentFragment(),
+    pageActionsSubTemplate = dom.find('.new-page-actions').cloneNode(true);
+
+  // append actions to the doc fragment
+  innerEl.appendChild(pageActionsSubTemplate);
+  // create the root pane element
+  open(header, innerEl);
+  // init controller for publish pane
+  ds.controller('pane-new-page', newPagePaneController);
+  return ds.get('pane-new-page', pageActionsSubTemplate);
+}
+
 function addPreview(preview) {
   if (preview) {
     return `<span class="error-preview">${preview}</span>`;
@@ -157,16 +178,30 @@ function addPreview(preview) {
   }
 }
 
+/**
+ * format and assemble error messages
+ * @param {Object[]} errors
+ * @returns {Element}
+ */
 function addErrors(errors) {
   return _.reduce(errors, function (el, error) {
-    var errorEl = dom.create(`
-        <div class="publish-error">
-          <span class="label">${error.rule.label}:</span>
-          <span class="description">${error.rule.description}</span>
-          <ul class="errors"></ul>
-        </div>
-      `),
+    var errorEl = dom.find('.publish-errors'),
+      errorLabel = dom.find(errorEl, '.label'),
+      errorDescription = dom.find(errorEl, '.description'),
       list = dom.find(errorEl, 'ul');
+
+    // reset template label default if available
+    if (errorLabel && _.get(error, 'rule.label')) {
+      dom.replaceElement(errorLabel, dom.create(`<span class="label">${error.rule.label}:</span>`));
+    }
+
+    // reset template description default if available
+    if (errorDescription && _.get(error, 'rule.description')) {
+      dom.replaceElement(errorDescription, dom.create(`<span class="description">${error.rule.description}</span>`));
+    }
+
+    // remove default error messages
+    dom.clearChildren(list);
 
     // add each place where the error occurs
     _.each(error.errors, function (item) {
@@ -192,15 +227,10 @@ function addErrors(errors) {
  */
 function openValidationErrors(errors) {
   var header = 'Before you can publish&hellip;',
-    messageEl = dom.create(`
-      <div class="error-message">This page is missing things needed to publish.<br />Address the following and try publishing again.</div>
-    `),
     errorsEl = addErrors(errors),
     innerEl = document.createDocumentFragment();
 
-  innerEl.appendChild(messageEl);
   innerEl.appendChild(errorsEl);
-
   open(header, innerEl);
 }
 
@@ -224,6 +254,7 @@ function takeOffEveryZig() {
 
 module.exports.close = close;
 module.exports.open = open;
+module.exports.openNewPage = openNewPage;
 module.exports.openPublish = openPublish;
 module.exports.openValidationErrors = openValidationErrors;
 module.exports.takeOffEveryZig = takeOffEveryZig;
--- services/pane.test.js
+++ services/pane.test.js
@@ -1,5 +1,6 @@
 var dirname = __dirname.split('/').pop(),
   filename = __filename.split('/').pop().split('.').shift(),
+  edit = require('./edit'),
   lib = require('./pane'),
   state = require('./page-state'),
   ds = require('dollar-slice');
@@ -156,54 +157,106 @@ describe(dirname, function () {
       });
     });
 
-    describe('openValidationErrors', function () {
-      var fn = lib[this.title],
-        sandbox;
+    describe('openNewPage', function () {
+      var mock = {
+          locals: {edit: true}
+        },
+        noFilter = function () {},
+        createPage, el, env, pagePane, result, sandbox, stub, template;
 
       before(function () {
-        var template = document.createElement('template'),
-          headEl = document.createElement('div'),
-          paneInnerEl = document.createElement('div'),
-          toolbar = document.createElement('div');
-
-        // add template
-        template.classList.add('kiln-pane-template');
-        headEl.classList.add('pane-header');
-        paneInnerEl.classList.add('pane-inner');
-        template.content.appendChild(headEl);
-        template.content.appendChild(paneInnerEl);
-        document.body.appendChild(template);
-
-        // add toolbar
-        toolbar.classList.add('kiln-toolbar');
-        document.body.appendChild(toolbar);
+        env = new nunjucks.Environment();
+        // satisfy request for nunjucks filter in template
+        env.addFilter('includeFile', noFilter); // TODO – include nunjucks filters
+        template = env.getPreprocessedTemplate('template.nunjucks');
       });
 
       beforeEach(function () {
+        result = template.render(mock);
         sandbox = sinon.sandbox.create();
         sandbox.stub(ds);
-        sandbox.stub(state, 'get');
       });
 
       afterEach(function () {
+        document.body.innerHTML = undefined;
+        el = undefined;
+        sandbox.restore();
+      });
+
+      it('has a template skeleton for selecting page type', function () {
+        document.body.innerHTML += result;
+        el = document.querySelector('.new-page-actions');
+        expect(el).to.exist;
+      });
+
+      it('has a toolbar button for opening the page type selection dialog', function () {
+        document.body.innerHTML += result;
+        el = document.querySelector('.kiln-toolbar-button.new');
+        expect(el).to.exist;
+      });
+
+      it('creates a clone of the pane template on [+ page] button click', function () {
+        lib.close();
+        document.body.innerHTML += result;
+        el = document.querySelector('.kiln-toolbar-button.new');
+        sandbox.stub(el, 'click', function () { // TODO – remove stub once filters are functional
+          pagePane = document.querySelector('.kiln-pane-template-elements .new-page-actions');
+        });
+        el.click();
+
+        function clickToOpenPane() {
+          expect(pagePane).to.exist;
+        }
+
+        clickToOpenPane();
+      });
+
+      it('opens a create page pane and clicks new article page button', sinon.test(function () {
+        lib.close();
+        document.body.innerHTML += result;
+        el = document.querySelector('.new-page-actions .create-article-page');
+        stub = sandbox.stub(el, 'click');
+        el.click();
+        sinon.assert.called(stub);
+      }));
+
+      it('creates a new article page', sinon.test(function () {
+        createPage = sandbox.spy(edit, 'createPage');
+        lib.close();
+        expect(createPage.returned(Promise.resolve({}))).to.exist;
+      }));
+    });
+
+    describe('openValidationErrors', function () {
+      var mock = {
+          locals: {edit: true}
+        },
+        noFilter = function () {},
+        el, env, result, sandbox, template;
+
+      before(function () {
+        env = new nunjucks.Environment();
+        // satisfy request for nunjucks filter in template
+        env.addFilter('includeFile', noFilter); // TODO – include nunjucks filters
+        template = env.getPreprocessedTemplate('template.nunjucks');
+      });
+
+      beforeEach(function () {
+        result = template.render(mock);
+        sandbox = sinon.sandbox.create();
+        sandbox.stub(ds);
+      });
+
+      afterEach(function () {
+        document.body.innerHTML = undefined;
+        el = undefined;
         sandbox.restore();
       });
 
       it('opens an error pane', function () {
-        lib.close();
-
-        function expectPane() {
-          expect(document.querySelector('.pane-inner .error-message')).to.not.equal(null);
-        }
-
-        fn([{
-          errors: [{ preview: 'ugh', label: 'no' }],
-          rule: {
-            label: 'no',
-            description: 'bad data'
-          }
-        }]);
-        expectPane();
+        document.body.innerHTML += result;
+        el = document.querySelector('.publish-errors');
+        expect(el).to.exist;
       });
     });
   });
