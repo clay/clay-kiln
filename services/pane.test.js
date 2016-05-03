1d3e524a7108c9f647af805cde3e9db3edb4881d
Nelson Pecora <nelson@yoshokatana.com>
2016-05-03 14:57:04 -0400

    added tests for publish warnings

--- services/pane.test.js
+++ services/pane.test.js
@@ -65,10 +65,12 @@ describe(dirname, function () {
       sandbox = sinon.sandbox.create();
       getTemplate = sandbox.stub(lib, 'getTemplate');
       getTemplate.withArgs('.kiln-pane-template').returns(stubWrapperTemplate());
+      getTemplate.withArgs('.publish-valid-template').returns(dom.create('<div class="publish-valid">valid</div>'));
       getTemplate.withArgs('.publish-messages-template').returns(stubMessageTemplate());
       getTemplate.withArgs('.publish-actions-template').returns(stubPublishTemplate());
       getTemplate.withArgs('.new-page-actions-template').returns(stubNewPageActionsTemplate());
       getTemplate.withArgs('.publish-error-message-template').returns(dom.create('<div>ERROR MESSAGE</div>'));
+      getTemplate.withArgs('.publish-warning-message-template').returns(dom.create('<div>WARNING MESSAGE</div>'));
       getTemplate.withArgs('.publish-errors-template').returns(stubErrorsTemplate());
     });
 
@@ -195,6 +197,42 @@ describe(dirname, function () {
         }
 
         fn().then(expectScheduledPane);
+      });
+
+      it('adds valid message if there are no warnings', function () {
+        getState.returns(Promise.resolve({}));
+        lib.close();
+
+        function expectRegularPane() {
+          expect(document.querySelector('.pane-header').innerHTML).to.equal('Schedule Publish');
+          expect(document.querySelector('.pane-inner .publish-valid').innerHTML).to.equal('valid');
+        }
+
+        fn().then(expectRegularPane);
+      });
+
+      it('adds warning message if warnings are passed in', function () {
+        getState.returns(Promise.resolve({}));
+        lib.close();
+
+        function expectRegularPane() {
+          expect(document.querySelector('.pane-header').innerHTML).to.equal('Schedule Publish');
+          expect(document.querySelector('.pane-inner .publish-valid')).to.equal(null);
+          expect(document.querySelector('.pane-inner .publish-warning .label').innerHTML).to.equal('Wrong:'); // note the semicolon
+          expect(document.querySelector('.pane-inner .publish-warning .description').innerHTML).to.equal('Way');
+          expect(document.querySelectorAll('.pane-inner .errors li').length).to.equal(1);
+        }
+
+        fn([{
+          rule: {
+            label: 'Wrong',
+            description: 'Way'
+          },
+          errors: [{
+            label: 'Foo',
+            preview: 'Bar'
+          }]
+        }]).then(expectRegularPane);
       });
     });
 
