0a9685b177a41e09e4a6cb58cc11e51a5e4a0d78
Nelson Pecora <nelson@yoshokatana.com>
2016-05-03 13:17:45 -0400

    updaated validators service

--- validators/index.js
+++ validators/index.js
@@ -1,9 +1,13 @@
 // array of all publishing rules, added to global
 window.kiln = window.kiln || {}; // create global kiln if it doesn't exist
-window.kiln.validators = window.kiln.validators || [];
+window.kiln.validators = window.kiln.validators || { errors: [], warnings: [] }; // create global validators if they don't exist
 
-module.exports = window.kiln.validators;
+module.exports = window.kiln.validators; // export them to use in other services
 
-module.exports.add = function (rule) {
-  window.kiln.validators.push(rule);
+module.exports.addError = function (rule) {
+  window.kiln.validators.errors.push(rule);
+};
+
+module.exports.addWarning = function (rule) {
+  window.kiln.validators.warnings.push(rule);
 };
