3d396f63388a2f84dd237e63af4a8224782224cf
Nelson Pecora <nelson@yoshokatana.com>
2016-08-22 15:54:16 -0400

    validate custom urls (#596)
    * validate custom urls
    
    * safer route checking
    
    * remove url message
    
    * simpler status message

--- controllers/publish-pane.js
+++ controllers/publish-pane.js
@@ -1,4 +1,4 @@
-var moment = require('moment'),
+const moment = require('moment'),
   pane = require('../services/pane'),
   edit = require('../services/edit'),
   progress = require('../services/progress'),
@@ -6,7 +6,8 @@ var moment = require('moment'),
   state = require('../services/page-state'),
   db = require('../services/edit/db'),
   site = require('../services/site'),
-  _ = require('lodash');
+  _ = require('lodash'),
+  Routable = require('routable');
 
 /**
  * schedule page and layout publishing in parallel
@@ -45,18 +46,45 @@ function unschedulePageAndLayout() {
   });
 }
 
+/**
+ * get available routes from the toolbar
+ * @returns {array}
+ */
+function getAvailableRoutes() {
+  var toolbar = dom.find('.kiln-toolbar'),
+    routes = toolbar && _.isString(toolbar.getAttribute('data-routes')) && toolbar.getAttribute('data-routes').split(',');
+
+  return routes || [];
+}
+
+/**
+ * test to see if a url is routable on the current site
+ * @param {string} val
+ * @param {array} routes
+ * @returns {boolean}
+ */
+function isValidUrl(val, routes) {
+  return !!_.find(routes, function (route) {
+    var r = new Routable(route);
+
+    return r.test(val) || r.test('/' + val); // test with and without the beginning slash
+  });
+}
+
 module.exports = function () {
-  function constructor(el) {
+  function Constructor(el) {
     this.form = dom.find(el, '.schedule');
     this.customUrlForm = dom.find(el, '.custom-url-form');
+    this.routes = getAvailableRoutes();
   }
 
-  constructor.prototype = {
+  Constructor.prototype = {
     events: {
       '.publish-now click': 'onPublishNow',
       '.unpublish click': 'onUnpublish',
       '.schedule submit': 'onSchedule',
       '.unschedule click': 'onUnschedule',
+      '.custom-url-input input': 'onCustomUrlInput',
       '.custom-url-form submit': 'onCustomUrl'
     },
 
@@ -153,6 +181,21 @@ module.exports = function () {
         });
     },
 
+    onCustomUrlInput: function (e) {
+      var input = e.currentTarget,
+        val = input.value,
+        routes = this.routes;
+
+      // validate that what the user typed in is routable
+      // note: if it's empty string, catch it early (removing custom urls is totally valid)
+      // note: if it's a full url, assume the user knows what they're doing and say it's valid
+      if (val === '' || val.match(/^http/i) || isValidUrl(val, routes)) {
+        input.setCustomValidity('');
+      } else {
+        input.setCustomValidity('Custom URL must match an available route!');
+      }
+    },
+
     onCustomUrl: function (e) {
       var val = dom.find(this.customUrlForm, 'input').value,
         url;
@@ -185,7 +228,13 @@ module.exports = function () {
         })
         .then(function () {
           progress.done('page');
-          progress.open('page', 'Saved custom page url', true);
+          if (url) {
+            // if we're saving, say that
+            progress.open('page', 'Saved custom page url', true);
+          } else {
+            // if we're explicitly removing a custom url, say that
+            progress.open('page', 'Removed custom page url', true);
+          }
         })
         .catch(function () {
           progress.done('error');
@@ -193,5 +242,5 @@ module.exports = function () {
         });
     }
   };
-  return constructor;
+  return Constructor;
 };
