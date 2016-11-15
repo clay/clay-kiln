0222c87266a7bd2a88dfa2875e85e81f45ff93ab
Nelson Pecora <nelson@yoshokatana.com>
2016-11-15 10:46:42 -0500

    added fake pages

--- services/pane/clay-menu.js
+++ services/pane/clay-menu.js
@@ -2,14 +2,19 @@ const ds = require('dollar-slice'),
   _ = require('lodash'),
   tpl = require('../tpl'),
   pane = require('./'),
+  site = require('../site'),
+  dom = require('@nymag/dom'),
   clayMenuController = require('../../controllers/clay-menu');
 
 function openClayMenu() {
-  var el = pane.open([/* todo: add My Pages and All Pages tabs */], {
-    header: 'Settings',
-    content: tpl.get('.settings-tab-template'),
-    active: true // todo: in the future, default to "My Pages"
-  }, 'left');
+  var pageContent = dom.create(`<img class="fakey" src="${site.get('assetPath')}/media/components/clay-kiln/fakey.png" />`),
+    el = pane.open([{
+      header: 'Pages',
+      content: pageContent
+    }], {
+      header: 'Settings',
+      content: tpl.get('.settings-tab-template')
+    }, 'left');
 
   ds.controller('clay-menu', clayMenuController);
   ds.get('clay-menu', el);
