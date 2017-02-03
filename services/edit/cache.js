cf7bd178f1038e4db8bd9d0d8375c94bcf9310a8
Nelson Pecora <nelson@keats.me>
2017-02-03 15:36:03 -0500

    preload data (#754)
    * preload data
    
    * added comment
    
    * default, just in case

--- services/edit/cache.js
+++ services/edit/cache.js
@@ -190,6 +190,29 @@ function createThrough(uri, data) {
   });
 }
 
+/**
+ * preload data for components on the page
+ * this iterates through a tree of data and warms the getDataOnly cache for all components found
+ * @param  {*} tree
+ */
+function preloadData(tree) {
+  if (_.isObject(tree) && tree._ref) {
+    // warm cache if we found a component!
+    exports.getDataOnly.cache.set(tree._ref, tree);
+  }
+
+  if (_.isObject(tree)) {
+    _.forOwn(tree, function (val, key) {
+      if (!_.includes(key, '_') && !_.includes(['blockParams', 'filename', 'knownHelpers', 'locals', 'media', 'site', 'state', 'template'])) {
+        // don't iterate through any of the templating/amphora metadata or locals/state
+        preloadData(val);
+      }
+    });
+  } else if (_.isArray(tree)) {
+    _.each(tree, preloadData);
+  }
+}
+
 // remembers
 exports.getData = control.memoizePromise(getData);
 exports.getDataOnly = control.memoizePromise(getDataOnly);
@@ -208,3 +231,8 @@ exports.clearSchemaCache = function () {
 exports.removeExtras = removeExtras; // testing client-side models
 
 _.set(window, 'kiln.services.schemaCache', schemaCache);
+
+// preload data on page load
+if (window.kiln.services.preloadData) {
+  preloadData(window.kiln.services.preloadData);
+}
