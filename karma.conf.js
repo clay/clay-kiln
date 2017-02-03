c5d207bf1925eb91662a23a9c9d7c65663a470ab
Nelson Pecora <nelson@yoshokatana.com>
2017-02-03 10:56:43 -0500

    re-add browserstack

--- karma.conf.js
+++ karma.conf.js
@@ -8,7 +8,7 @@ const files = [
     preprocessors: {
       'test/setup.js': ['webpack', 'sourcemap']
     },
-    frameworks: ['mocha', 'sinon', 'chai'],
+    frameworks: ['mocha', 'sinon-chai'],
     webpack: {
       target: 'web',
       module: {
@@ -35,6 +35,46 @@ const files = [
         lines: [50, 75]
       }
     },
+    browserStack: {
+      username: process.env.BROWSERSTACK_USERNAME,
+      accessKey: process.env.BROWSERSTACK_ACCESS_KEY,
+      retryLimit: 0 // yolo
+    },
+    customLaunchers: {
+      chromeMac: {
+        base: 'BrowserStack',
+        browser: 'chrome',
+        browser_version: '55',
+        os: 'OS X',
+        os_version: 'Sierra'
+      },
+      firefoxMac: {
+        base: 'BrowserStack',
+        browser: 'firefox',
+        browser_version: '50',
+        os: 'OS X',
+        os_version: 'Sierra'
+      },
+      chromeWindows: {
+        base: 'BrowserStack',
+        browser: 'chrome',
+        browser_version: '55',
+        os: 'Windows',
+        os_version: '10'
+      },
+      firefoxWindows: {
+        base: 'BrowserStack',
+        browser: 'firefox',
+        browser_version: '50',
+        os: 'Windows',
+        os_version: '10'
+      }
+      // note: edge doesn't support Element#matches
+      // note: safari has a bug where it reloads the page (chrome and firefox do not)
+      // note: phantomjs doesn't support Promises
+      // note: mobile safari passes all tests, but runs some of them more than once(!!)
+    },
+    browsers: ['chromeMac', 'firefoxMac', 'chromeWindows', 'firefoxWindows'],
     files: files
   };
 
