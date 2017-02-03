87e1cc8a86d69abdf83a2ac02cf338862d93bb45
Nelson Pecora <nelson@keats.me>
2017-02-03 16:17:06 -0500

    add partials for all component templates (#755)
    * add partials for all component templates
    
    * check to see if templates exist

--- services/edit/index.js
+++ services/edit/index.js
@@ -18,6 +18,17 @@ var _ = require('lodash'),
   hbs = require('nymag-handlebars')(),
   pt = require('promise-timeout');
 
+// add precompiled templates as partials (on page load)
+document.addEventListener('DOMContentLoaded', function () {
+  const templates = _.get(window, 'kiln.services.componentTemplates');
+
+  if (_.isObject(templates) && !_.isEmpty(templates)) {
+    _.forOwn(templates, function (tpl, name) {
+      hbs.registerPartial(name, hbs.template(tpl));
+    });
+  }
+});
+
 /**
  * Cloning removes extra properties like _schema from standard types like Array, because we're doing a bad thing.
  *
