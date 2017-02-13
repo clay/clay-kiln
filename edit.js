953b04dfc01d6906156daf342761a34a6fabd0b6
Nelson Pecora <nelson@yoshokatana.com>
2017-02-13 17:57:35 -0500

    unselect when clicking out

--- edit.js
+++ edit.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import Vue from 'vue';
 import store from './lib/core-data/store';
 import { decorateAll } from './lib/decorators';
@@ -23,4 +24,13 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   store.dispatch('preload').then(() => decorateAll());
+
+  // when clicks bubble up to the document, close the current form or pane / unselect components
+  document.body.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    if (_.get(store, 'state.ui.currentSelection')) {
+      store.dispatch('unselect');
+    }
+  });
 });
