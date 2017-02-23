31798ea53b03bcb452d87889869f700e225c3958
Jon Winton <winton.jon@gmail.com>
2017-02-23 16:52:44 -0500

    Pulling list from new-pages endpoint

--- panes/new-page.vue
+++ panes/new-page.vue
@@ -4,18 +4,26 @@
 </style>
 
 <template>
-  <filterable-list :content="content" :onClick="itemClick"></filterable-list>
+  <filterable-list :content="pages" :onClick="itemClick"></filterable-list>
 </template>
 
 
 <script>
   import _ from 'lodash';
+  import { getJSON } from '../lib/core-data/api';
+  import { uriToUrl } from '../lib/utils/urls';
   import filterableList from './filterable-list.vue';
 
   export default {
     props: ['content'],
     data() {
       return {}
+    },
+    asyncComputed: {
+      pages() {
+        // Get the JSON response from the API passed in
+        return getJSON(`${uriToUrl(this.$store.state.site.prefix)}/lists/new-pages`);
+      }
     },
     methods: {
       itemClick(id) {
