f8d1b8e1f201bae280dddee4ea379c1c393668e9
Nelson Pecora <nelson@yoshokatana.com>
2017-03-24 13:26:19 -0400

    reorder in invisible list

--- panes/invisible-components.vue
+++ panes/invisible-components.vue
@@ -1,5 +1,5 @@
 <template>
-  <filterable-list :content="components" :placeholder="placeholder" :onSettings="openSettings" :onDelete="removeComponent"></filterable-list>
+  <filterable-list :content="list" :placeholder="placeholder" :onSettings="openSettings" :onDelete="removeComponent" :onReorder="reorderComponents"></filterable-list>
 </template>
 
 <script>
@@ -31,7 +31,9 @@
       return {};
     },
     computed: {
-      components() {
+      list() {
+        // note: invisible components are currently only allowed in the layout data
+        // todo: allow these in page data as well?
         return _.map(_.get(this.$store, `state.components['${this.args.uri}'].${this.args.path}`), formatComponents);
       },
       label() {
@@ -52,6 +54,13 @@
         const componentEl = find(`[${refAttr}="${id}"]`);
 
         this.$store.dispatch('removeComponent', componentEl);
+      },
+      reorderComponents(id, index, oldIndex) {
+        let componentList = _.reduce(_.get(this.$store, `state.components['${this.args.uri}'].${this.args.path}`), (list, val) => list.concat({ [refProp]: val[refProp] }), []);
+
+        componentList.splice(oldIndex, 1); // remove at the old index
+        componentList.splice(index, 0, { [refProp]: id }); // add at the new index
+        this.$store.dispatch('saveComponent', { uri: this.args.uri, data: { [this.args.path]: componentList }});
       }
     },
     components: {
