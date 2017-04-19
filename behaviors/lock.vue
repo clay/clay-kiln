aa0ecc47c4b0749280bae3231d201d732f7d34e7
Nelson Pecora <nelson@yoshokatana.com>
2017-04-19 16:28:07 -0400

    stop lock click from closing forms

--- behaviors/lock.vue
+++ behaviors/lock.vue
@@ -63,7 +63,7 @@
 </style>
 
 <template>
-  <button class="lock-button" :class="{ unlocked: !locked }" @click.stop="toggleLock">
+  <button class="lock-button" :class="{ unlocked: !locked }" @click.stop.prevent="toggleLock">
     <icon name="locked" class="locked"></icon>
     <icon name="unlocked" class="unlocked"></icon>
   </button>
