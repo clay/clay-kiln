7bfbd10f152d4f7b75aa22e737f04ade8a1740ee
Nelson Pecora <nelson@yoshokatana.com>
2017-05-02 16:49:50 -0400

    destructured wysiwyg caret

--- behaviors/wysiwyg.vue
+++ behaviors/wysiwyg.vue
@@ -139,7 +139,6 @@
   import Quill from 'quill';
   import _ from 'lodash';
   import { find } from '@nymag/dom';
-  import striptags from 'striptags';
   import { getComponentName, refAttr, editAttr } from '../lib/utils/references';
   import { UPDATE_FORMDATA } from '../lib/forms/mutationTypes';
   import { getPrevComponent, getNextComponent, getParentComponent, getComponentEl } from '../lib/utils/component-elements';
@@ -147,6 +146,7 @@
   import { sanitizeInlineHTML, sanitizeMultiComponentHTML, sanitizeBlockHTML } from './wysiwyg-sanitize';
   import { splitParagraphs, matchComponents, generatePasteRules } from './wysiwyg-paste';
   import { renderDeltas, generateDeltas, deltaEndsWith, matchLineBreak, matchParagraphs } from './wysiwyg-deltas';
+  import { getNewlinesBeforeCaret, getLastOffsetWithNewlines } from './wysiwyg-caret';
 
   const Delta = Quill.import('delta'),
     Clipboard = Quill.import('modules/clipboard'),
@@ -191,54 +191,6 @@
     }
 
     return delta;
-  }
-
-  /**
-   * get the number of newlines before the caret,
-   * so we can accurately set the caret offset when clicking into multiline wysiwyg fields
-   * @param  {string} data
-   * @param  {number} offset
-   * @return {number}
-   */
-  function getNewlinesBeforeCaret(data, offset) {
-    const text = (data || '').replace(/(<\/p><p>|<br \/>)/ig, '\u00B6'), // convert to ¶ so we have something to count
-      plainText = striptags(text);
-
-    let i = 0,
-      realOffset = offset;
-
-    // go through each character (up to the real offset),
-    // increasing the real offset every time we hit a paragraph
-    for (; i <= realOffset; i++) {
-      if (plainText[i] === '\u00B6') {
-        realOffset++; // also increase the temp offset, since paragraph breaks didn't count in the initial offset
-      }
-    }
-
-    return realOffset;
-  }
-
-  /**
-   * get last offset (caret at the END of some data), when the data has newlines
-   * @param {string} data
-   * @return {number}
-   */
-  function getLastOffsetWithNewlines(data) {
-    const text = (data || '').replace(/(<\/p><p>|<br \/>)/ig, '\u00B6'), // convert to ¶ so we have something to count
-      plainText = striptags(text);
-
-    let i = 0,
-      realOffset = plainText.length - 1;
-
-    // go through each character (up to the end of the text),
-    // increasing the real offset every time we hit a paragraph
-    for (; i <= realOffset; i++) {
-      if (plainText[i] === '\u00B6') {
-        realOffset++;
-      }
-    }
-
-    return realOffset;
   }
 
   /**
