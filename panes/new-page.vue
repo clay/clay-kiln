5c10bf126bddabb76d7e26c8ad6f53d0d050cb52
Nelson Pecora <nelson@yoshokatana.com>
2017-08-03 16:44:26 -0400

    autosort new page list per site

--- panes/new-page.vue
+++ panes/new-page.vue
@@ -9,7 +9,10 @@
 
 
 <script>
+  import _ from 'lodash';
   import { getObject } from '../lib/core-data/api';
+  import { getItem, updateArray } from '../lib/utils/local';
+  import { props } from '../lib/utils/promises';
   import filterableList from './filterable-list.vue';
 
   export default {
@@ -19,14 +22,24 @@
     },
     asyncComputed: {
       pages() {
-        // Get the JSON response from the API passed in
-        return getObject(`${this.$store.state.site.prefix}/lists/new-pages`);
+        return props({
+          pages: getObject(`${this.$store.state.site.prefix}/lists/new-pages`),
+          sortList: getItem(`newpages:${this.$store.state.site.slug}`)
+        }).then(({ pages, sortList }) => {
+          sortList = sortList || [];
+          const sorted = _.intersectionBy(sortList, pages, 'id'),
+            unsorted = _.differenceBy(pages, sortList, 'id');
+
+          return sorted.concat(unsorted);
+        });
       }
     },
     methods: {
       itemClick(id, title) {
-        this.$store.commit('CREATE_PAGE', title);
-        this.$store.dispatch('createPage', id).then((url) => window.location.href = url);
+        return updateArray(`newpages:${this.$store.state.site.slug}`, { id, title }, 'id')
+          .then(() => this.$store.commit('CREATE_PAGE', title))
+          .then(() => this.$store.dispatch('createPage', id))
+          .then((url) => window.location.href = url);
       }
     },
     components: {
