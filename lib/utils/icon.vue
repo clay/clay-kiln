aa9c2cb4b6db95857a85ac97407536ae1fdfe493
Nelson Pecora <nelson@yoshokatana.com>
2017-09-21 17:06:39 -0400

    use ui-icon

--- lib/utils/icon.vue
+++ lib/utils/icon.vue
@@ -1,10 +1,12 @@
 <template>
-  <span class="icon" v-html="icon"></span>
+  <ui-icon v-html="icon"></ui-icon>
 </template>
 
 <script>
   import icons from './icons';
+  import UiIcon from 'keen-ui/src/UiIcon.vue';
 
+  // NOTE: this is only for using custom SVG icons. For material design icons, please use ui-icon directly
   export default {
     props: ['name'],
     data() {
@@ -14,6 +16,9 @@
       icon() {
         return icons[this.name];
       }
+    },
+    components: {
+      UiIcon
     }
   };
 </script>
