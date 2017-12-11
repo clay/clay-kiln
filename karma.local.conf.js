fcc0b55fda5c044e8d4809f2371763b184c8c746
Nelson Pecora <nelson@yoshokatana.com>
2017-12-11 17:48:22 -0500

    added new validation helpers

--- karma.local.conf.js
+++ karma.local.conf.js
@@ -1,5 +1,4 @@
 var settings = require('./karma.conf.js').settings,
-  WatchIgnorePlugin = require('webpack').WatchIgnorePlugin,
   _ = require('lodash');
 
 // generating code coverage reports for local tests
@@ -10,19 +9,17 @@ settings.coverageReporter.reporters = [
 
 module.exports = function (karma) {
   let localSettings = _.assign(settings, {
-    singleRun: false,
-    autoWatch: true,
-    autoWatchBatchDelay: 2000,
+    singleRun: true,
+    // autoWatch: false,
+    // autoWatchBatchDelay: 2000,
     browsers: ['Chrome']
   });
 
   _.set(localSettings, 'webpack.watch', true);
-  _.set(localSettings, 'webpack.plugins', [
-    new WatchIgnorePlugin([
-      /node_modules\//ig,
-      /coverage\//ig,
-      /dist\//ig
-    ])
-  ]);
+  _.set(localSettings, 'webpack.watchOptions', {
+    aggregateTimeout: 300,
+    ignored: /(node_modules|coverage|dist|docs|media|styleguide|.git|.github)/,
+    poll: 1000
+  });
   karma.set(localSettings);
 };
