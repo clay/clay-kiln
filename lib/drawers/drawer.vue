4e4346d53cc1be59c9a370beb8af647c106fc866
Byron Hulcher <byronhulcher@gmail.com>
2018-02-05 17:01:39 -0500

    Add Page History drawer (#1104)
    * Added Page History drawer
    
    * Linting
    
    * Pull from store when mounting the page history drawer
    
    * Updated Designs
    
    * Fix tests

--- lib/drawers/drawer.vue
+++ lib/drawers/drawer.vue
@@ -73,6 +73,7 @@
   import UiTab from 'keen/UiTab';
   // components used in tabs
   import contributors from './contributors.vue';
+  import pageHistory from './page-history.vue';
   import visibleComponents from './visible-components.vue';
   import headComponents from './head-components.vue';
   import invisibleComponents from './invisible-components.vue';
@@ -147,6 +148,9 @@
             title: 'Contributors',
             component: 'contributors',
             selected: true
+          },{
+            title: 'Page History',
+            component: 'page-history'
           }];
         } else if (this.name === 'components') {
           const state = _.get(this.$store, 'state'),
@@ -204,6 +208,7 @@
       UiTabs,
       UiTab,
       contributors,
+      'page-history': pageHistory,
       'visible-components': visibleComponents,
       'head-components': headComponents,
       'invisible-components': invisibleComponents,
