eb643d049efa493f7de85be3f28aa87ddb6d4a74
Manuel Emilio Urena <manuel.ureh@hotmail.com>
2019-05-15 12:08:07 -0400

    Applies PR code review feedback

--- lib/nav/sort-selector.vue
+++ lib/nav/sort-selector.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="sort-selector" :class="{ 'is-vertical': vertical }">
-    <ui-radio class="sort-selector-radio" color="accent" label="Recently Updated" :value="selectedSort" trueValue="updateTime" @input="input" />
-    <ui-radio class="sort-selector-radio" color="accent" label="Recently Published" :value="selectedSort" trueValue="publishTime" @input="input" />
-    <ui-radio class="sort-selector-radio" color="accent" label="Recently Scheduled" :value="selectedSort" trueValue="scheduledTime" @input="input" />
+    <ui-radio
+      v-for="(sortItem) in sortingValues"
+      :key="sortItem.trueValue"
+      class="sort-selector-radio"
+      color="accent"
+      :label="sortItem.label"
+      :value="selectedSort"
+      :trueValue="sortItem.trueValue"
+      @input="input"
+    />
   </div>
 </template>
 
@@ -18,6 +25,20 @@
     },
     components: {
       UiRadio
+    },
+    data() {
+      return {
+        sortingValues: [{
+          label: 'Recently Updated',
+          trueValue: 'updateTime'
+        }, {
+          label: 'Recently Published',
+          trueValue: 'publishTime'
+        }, {
+          label: 'Recently Scheduled',
+          trueValue: 'scheduledTime'
+        }]
+      };
     }
   };
 </script>
