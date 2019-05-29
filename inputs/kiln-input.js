01eb480a13822b9cf842029add272410ef4a9bd6
Scott Nash <scott.nash@nymag.com>
2019-05-29 09:36:56 -0400

    changes based on pr feedback as well as a fix for a race condition

--- inputs/kiln-input.js
+++ inputs/kiln-input.js
@@ -1,9 +1,13 @@
 import store from '../lib/core-data/store';
 import * as api from '../lib/core-data/api.js';
 import { replaceVersion } from 'clayutils';
-import _ from 'lodash';
+import _cloneDeep from 'lodash/cloneDeep';
 
 export default class KilnInput {
+  /**
+  * @param {object} schema
+  * @param {string} inputName
+  */
   constructor(schema, inputName) {
     Object.assign(this, {
       inputName,
@@ -13,10 +17,20 @@ export default class KilnInput {
     });
   };
 
+  /**
+   * fetchs and returns a component as a json object
+  * @param {string} uri
+  * @return {Promise}
+  */
   getComponentData(uri) {
     return api.getJSON(`http://${uri}`);
   }
 
+  /**
+   * returns an array of components with the passed name on the current page
+  * @param {string} componentName
+  * @return {array}
+  */
   getComponentInstances(componentName) {
     const components = store.state.components,
       instances = [];
@@ -30,15 +44,28 @@ export default class KilnInput {
     return instances;
   }
 
+  /**
+  * returns a copy of the current Vuex state
+  * @return {object}
+  */
   getState() {
-    return _.cloneDeep(store.state);
+    return _cloneDeep(store.state);
   }
 
+  /**
+  * register an event
+  * @param {string} event
+  * @param {function} fn
+  */
   on(event, fn) {
     this.events = this.events || {};
     this.events[event] = fn;
   }
 
+  /**
+  * hide the input
+  * @returns {promise}
+  */
   hide() {
     return new Promise((resolve) => {
       if (store.state.schemas[this.schemaName]) {
@@ -50,20 +77,44 @@ export default class KilnInput {
     });
   }
 
+  /**
+  * publish a component by saving to the @published version
+  * @param {string} uri
+  * @param {object} data
+  * @returns {promise}
+  */
   publishComponent(uri, data) {
-    this.saveComponent(replaceVersion(uri, 'published'), data);
+    return this.saveComponent(replaceVersion(uri, 'published'), data);
   }
 
+  /**
+  * re-render a component instance
+  * useful if a component's data has been updated by an outside source
+  * @param {string} uri
+  * @returns {promise}
+  */
   reRenderInstance(uri) {
-    api.getJSON(`http://${uri}`).then((component) => {
+    return api.getJSON(`http://${uri}`).then((component) => {
       store.dispatch('triggerModelRender', { uri, component });
     });
   }
 
+  /**
+  * save a component
+  * @param {string} uri
+  * @param {object} data
+  * @returns {promise}
+  */
   saveComponent(uri, data) {
     return store.dispatch('saveComponent', { uri, data });
   }
 
+  /**
+  * update the value of a prop on the current schema
+  * @param {string} prop
+  * @param {string|number|object} value
+  * @returns {promise}
+  */
   setProp(prop, value) {
     return new Promise((resolve) => {
       if (this[prop] !== value ) {
@@ -77,11 +128,21 @@ export default class KilnInput {
     });
   }
 
+  /**
+  * show the input
+  * @returns {promise}
+  */
   show() {
     // just an alias/shortcut for calling setProp('visibility', true);
     return this.setProp('visibility', true);
   }
 
+  /**
+  * save a component
+  * @param {string} event
+  * @param {function} fn
+  * @param {boolean} scoped
+  */
   subscribe(event, fn, scoped = false) {
     this.subscribedEvents[event] = {
       func: fn,
@@ -91,6 +152,9 @@ export default class KilnInput {
     this.subscribeToMutations();
   }
 
+  /**
+  * subscribe to vuex mutations
+  */
   subscribeToMutations() {
     if (this.unsubscribe) {
       this.unsubscribe();
@@ -103,21 +167,35 @@ export default class KilnInput {
     });
   }
 
+  /**
+  * check if this event is subscribed to
+  * @param {type} string
+  * @return {boolean}
+  */
   subscribedToEvent({ type }) {
     const event = this.subscribedEvents[type];
 
-    return event && typeof event.func === 'function' && (!event.scoped || !store.state.url || event.scoped && store.state.url.component === this.schemaName);
+    return event && typeof event.func === 'function' && (!event.scoped || event.scoped && store.state.url && store.state.url.component === this.schemaName);
   }
 
+  /**
+  * get the vuex url object, which contains the hash elements
+  * @return {object}
+  */
   url() {
     // JSONing out the Vue object into a standard JavaScript object
     return JSON.parse(JSON.stringify(store.state.url));
   }
 
+  /**
+  * Get or set the value for the input
+  * @param {string|number|object|array} val
+  * @return {string|number|object|array}
+  */
   value(val) {
     if (val) {
       // set the value of the field
-      store.dispatch('updateFormData', { path: this.inputName, val }).then(() => {
+      return store.dispatch('updateFormData', { path: this.inputName, val }).then(() => {
         return store.state.ui.currentForm ? store.state.ui.currentForm.fields[this.inputName] : null;
       });
     } else {
